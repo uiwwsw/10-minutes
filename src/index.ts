a079deefe471e161d5588bb057e378ac1a41d6f7
uiwwsw <uiwwsw@icloud.com>
2022-02-21 21:27:15 +0900

    ssss
    Signed-off-by: uiwwsw <uiwwsw@icloud.com>

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,75 @@
 import "./style.scss";
 import "phaser";
 // import logoImg from "./assets/logo.png";
+class FPSScene extends Phaser.Scene {
+  private text: Phaser.GameObjects.Text | null = null;
 
-export default class Demo extends Phaser.Scene {
-  logo: any;
-  kbd: Phaser.Types.Input.Keyboard.CursorKeys;
-  constructor(config: any) {
-    super(config);
+  constructor() {
+    super({ key: "fpsScene", active: true });
   }
 
-  preload() {
-    // this.load.image("logo", logoImg);
+  create(): void {
+    this.text = this.add.text(10, 10, "");
   }
 
-  create() {
-    // this.add.shader("RGB Shift Field", 0, 0, 800, 600).setOrigin(0);
-
-    // this.add.shader("Plasma", 0, 412, 800, 172).setOrigin(0);
-
-    // this.add.image(400, 300, "libs");
-
-    this.logo = this.add.image(400, 70, "logo");
-
-    // this.tweens.add({
-    //   targets: this.logo,
-    //   y: 350,
-    //   duration: 1500,
-    //   ease: "Sine.inOut",
-    //   yoyo: true,
-    //   repeat: -1,
-    // });
-    this.kbd = this.input.keyboard.createCursorKeys();
+  update(): void {
+    this.text.setText(`FPS: ${this.game.loop.actualFps}`);
   }
-  update() {
-    if (this.kbd.up.isDown) {
-      this.logo.y -= 1;
-    }
-    if (this.kbd.down.isDown) {
-      this.logo.y += 1;
-    }
-    if (this.kbd.left.isDown) {
-      this.logo.x -= 1;
-    }
-    if (this.kbd.right.isDown) {
-      this.logo.x += 1;
+}
+class MainScene extends Phaser.Scene {
+  private character: Phaser.GameObjects.Image | null = null;
+  // 방향키를 감지할 키를 추가하기!
+  private upKey: Phaser.Input.Keyboard.Key | null = null;
+  private downKey: Phaser.Input.Keyboard.Key | null = null;
+  private leftKey: Phaser.Input.Keyboard.Key | null = null;
+  private rightKey: Phaser.Input.Keyboard.Key | null = null;
+
+  constructor() {
+    super({ key: "main", active: true });
+  }
+
+  preload(): void {
+    this.load.image("character", "/assets/image/character.png");
+  }
+
+  create(): void {
+    this.character = this.add.image(100, 150, "character");
+    // 사용할 키를 추가해줍니다.
+    this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.downKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
+    );
+    this.leftKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.LEFT
+    );
+    this.rightKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.RIGHT
+    );
+  }
+
+  update(time: number, delta: number): void {
+    if (this.character) {
+      if (this.upKey?.isDown) this.character.y -= 10;
+      if (this.downKey?.isDown) this.character.y += 10;
+      if (this.leftKey?.isDown) this.character.x -= 10;
+      if (this.rightKey?.isDown) this.character.x += 10;
     }
   }
 }
-console.log(
-  getComputedStyle(document.querySelector(":root")).getPropertyValue(
-    "--background"
-  )
-);
-const config = {
-  type: Phaser.AUTO,
-  backgroundColor: "#125555",
-  width: 800,
-  height: 600,
-  scene: Demo,
-};
-
-const game = new Phaser.Game(config);
+const game: Phaser.Game = new Phaser.Game({
+  width: 1024,
+  height: 768,
+  scene: [MainScene, FPSScene],
+  // 물리 효과 추가
+  physics: {
+    default: "matter",
+    // matter 물리엔진 설정
+    matter: {
+      // 월드에 중력 설정
+      gravity: {
+        x: 0,
+        y: 9.8,
+      },
+    },
+  },
+});

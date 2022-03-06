36eecf15a99d1f9a678453dcbb4826ddbac9d6a0
uiwwsw <uiwwsw@icloud.com>
2022-03-06 13:04:52 +0900

    충돌 이벤트
    Signed-off-by: uiwwsw <uiwwsw@icloud.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "./style.scss";
 import "phaser";
-import Kakao from "./assets/image/ddd.jpeg";
+import Kakao2 from "./assets/image/ddd.jpeg";
+import Kakao1 from "./assets/image/1.png";
+import Kakao from "./assets/image/ic-system-service-binance.svg";
 class FPSScene extends Phaser.Scene {
   private text: Phaser.GameObjects.Text | null = null;
 
@@ -17,7 +19,7 @@ class FPSScene extends Phaser.Scene {
   }
 }
 class MainScene extends Phaser.Scene {
-  private character: Phaser.GameObjects.Image | null = null;
+  private character: Phaser.Physics.Matter.Image | null = null;
   // 방향키를 감지할 키를 추가하기!
   private upKey: Phaser.Input.Keyboard.Key | null = null;
   private downKey: Phaser.Input.Keyboard.Key | null = null;
@@ -30,13 +32,19 @@ class MainScene extends Phaser.Scene {
 
   preload(): void {
     this.load.image("character", Kakao);
+    this.load.image("character1", Kakao1);
+    this.load.image("character2", Kakao2);
   }
 
   create(): void {
     // this.matter.world.setBounds();
     // this.character2 = this.add.image(100, 150, "character");
-    this.character = this.add.image(100, 150, "character");
-    this.character.setScale(0.2);
+    this.character = this.matter.add.image(100, 150, "character");
+    const a = this.matter.add.image(300, 150, "character1");
+    const b = this.matter.add.image(500, 150, "character2");
+    a.setScale(0.2);
+    b.setScale(0.2);
+    this.matter.world.setBounds(0, 0, 800, 600);
 
     this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     this.downKey = this.input.keyboard.addKey(
@@ -48,30 +56,84 @@ class MainScene extends Phaser.Scene {
     this.rightKey = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.RIGHT
     );
+
+    this.matter.world.on("collisionstart", function (event) {
+      //  Loop through all of the collision pairs
+      var pairs = event.pairs;
+
+      for (var i = 0; i < pairs.length; i++) {
+        // var bodyA = pairs[i].bodyA;
+        // var bodyB = pairs[i].bodyB;
+        console.log(
+          pairs[i].bodyA.gameObject === a,
+          pairs[i].bodyB.gameObject === a
+        );
+        //  We only want sensor collisions
+        // if (pairs[i].isSensor) {
+        //   var blockBody;
+        //   var playerBody;
+
+        //   if (bodyA.isSensor) {
+        //     blockBody = bodyB;
+        //     playerBody = bodyA;
+        //   } else if (bodyB.isSensor) {
+        //     blockBody = bodyA;
+        //     playerBody = bodyB;
+        //   }
+
+        //   //  You can get to the Sprite via `gameObject` property
+        //   var playerSprite = playerBody.gameObject;
+        //   var blockSprite = blockBody.gameObject;
+
+        //   var color;
+
+        //   if (playerBody.label === "left") {
+        //     color = 0xff0000;
+        //   } else if (playerBody.label === "right") {
+        //     color = 0x00ff00;
+        //   } else if (playerBody.label === "top") {
+        //     color = 0x0000ff;
+        //   } else if (playerBody.label === "bottom") {
+        //     color = 0xffff00;
+        //   }
+
+        //   blockSprite.setTintFill(color);
+        // }
+      }
+    });
   }
 
-  update(time: number, delta: number): void {
+  update(): void {
     if (this.character) {
-      if (this.upKey?.isDown) this.character.y -= 10;
-      if (this.downKey?.isDown) this.character.y += 10;
-      if (this.leftKey?.isDown) this.character.x -= 10;
-      if (this.rightKey?.isDown) this.character.x += 10;
+      if (this.leftKey?.isDown) {
+        this.character.setVelocityX(-10);
+      } else if (this.rightKey?.isDown) {
+        this.character.setVelocityX(10);
+      } else {
+        this.character.setVelocityX(0);
+      }
+
+      if (this.upKey?.isDown) {
+        this.character.setVelocityY(-10);
+      } else if (this.downKey?.isDown) {
+        this.character.setVelocityY(10);
+      } else {
+        this.character.setVelocityY(0);
+      }
     }
   }
 }
 const game: Phaser.Game = new Phaser.Game({
-  width: 1024,
-  height: 768,
+  width: 800,
+  height: 600,
   scene: [MainScene, FPSScene],
   // 물리 효과 추가
   physics: {
     default: "matter",
-    // matter 물리엔진 설정
     matter: {
-      // 월드에 중력 설정
       gravity: {
         x: 0,
-        y: 9.8,
+        y: 0,
       },
     },
   },
